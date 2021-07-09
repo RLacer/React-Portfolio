b427094920f3f2b649acb83a324f0069f037a2e8
RLacer <r.a.lacer@hotmail.com>
2021-07-09 11:13:55 -0600

    adding content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,25 @@
-import "./about.scss"
+import "./about.scss";
 
 export default function about() {
-    return (
-        <div className="about" id="about">
-      
+  return (
+    <div className="about" id="about">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/images/bwoutdoorhead.png" alt="headshot" />
         </div>
-    )
+      </div>
+      <div className="right">
+          <div className="wrapper">
+              <h1>Welcome!</h1>
+              <h2>I'm Rebecca Lacer</h2>
+              <p>As an aspiring web developer
+              I enjoy developing my skills in full stack web dev.
+             I have a background in manufacturing quality assurance,
+            with a variety of skills.</p>
+          
+            <div><a href="#portfolio">Please Visit My Portfolio for Work Samples</a></div>
+          </div>
+      </div>
+    </div>
+  );
 }
